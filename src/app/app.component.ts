371d643cecec4df35cdbcc93e788b9cdedf883ff
Maximino Castillo <maxcastgarcia@gmail.com>
2024-10-23 01:23:04 -0600

    Add a testimonial-page component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TestimonialPageComponent } from "./testimonial-page/testimonial-page.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TestimonialPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/testimonial-page/testimonial-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-testimonial-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './testimonial-page.component.html',
+  styleUrl: './testimonial-page.component.scss'
+})
+export class TestimonialPageComponent {
+
+}
