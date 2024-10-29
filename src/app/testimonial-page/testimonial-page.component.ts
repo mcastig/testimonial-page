53cecbd13492a44134a19279d538bff297f63a57
Maximino Castillo <maxcastgarcia@gmail.com>
2024-10-29 03:01:14 -0600

    Complete cards for reviewers on right side

--- src/app/testimonial-page/testimonial-page.component.ts
+++ src/app/testimonial-page/testimonial-page.component.ts
@@ -8,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrl: './testimonial-page.component.scss'
 })
 export class TestimonialPageComponent {
+  star = 0;
 
+  starsGoogle = [
+    {id: 0, type: 'fill'},
+    {id: 1, type: 'fill'},
+    {id: 2, type: 'fill'},
+    {id: 3, type: 'fill'},
+    {id: 4, type: 'empty'}
+  ];
+
+  starsMeta = [
+    {id: 0, type: 'fill'},
+    {id: 1, type: 'fill'},
+    {id: 2, type: 'fill'},
+    {id: 3, type: 'fill'},
+    {id: 4, type: 'fill'}
+  ];
 }
